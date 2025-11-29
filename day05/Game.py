c982d58929b9aeb2b5076e7604be3730ba8883d9
noya791-cmyk <noya791@gmail.com>
2025-11-29 13:19:17 +0200

    lop added

--- day05/Game.py
+++ day05/Game.py
@@ -3,26 +3,23 @@ import random
 # Rock-Paper-Scissors Game
 options = ["rock", "paper", "scissors"]
 
-computer_choice = random.choice(options)
-player_choice = input("Enter rock, paper, or scissors: ").lower()
+while True:
+    computer_choice = random.choice(options)
+    player_choice = input("Enter rock, paper, or scissors: ").lower()
 
-if player_choice == computer_choice:
-    print(f"Both chose {player_choice}. It's a tie!")
-elif (
-    (player_choice == "rock" and computer_choice == "scissors")
-    or (player_choice == "paper" and computer_choice == "rock")
-    or (player_choice == "scissors" and computer_choice == "paper")
-):
-    print(
-        f"You chose {player_choice}, computer chose {computer_choice}. You win!"
-    )
-else:
-    print(
-        f"You chose {player_choice}, computer chose {computer_choice}. You lose!"
-    )
+    if player_choice == computer_choice:
+        print(f"Both chose {player_choice}. It's a tie!")
+    elif (
+        (player_choice == "rock" and computer_choice == "scissors")
+        or (player_choice == "paper" and computer_choice == "rock")
+        or (player_choice == "scissors" and computer_choice == "paper")
+    ):
+        print(f"You chose {player_choice}, computer chose {computer_choice}. You win!")
+    else:
+        print(f"You chose {player_choice}, computer chose {computer_choice}. You lose!")
 
-
-
-
-
-
+    # Ask to play again
+    again = input("Do you want to play again? (yes/no): ").lower()
+    if again != "yes":
+        print("Goodbye!")
+        break
